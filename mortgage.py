2b9fe1f24e73b636a23fad95d8e18b8d140820cf
Nick Pan <panick@panick-macbookpro3.roam.corp.google.com>
2020-04-03 15:18:49 -0400

    Complete Mortgage class... make_payment(),get_total_paid().

--- mortgage.py
+++ mortgage.py
@@ -8,11 +8,31 @@ def find_payment(loan, r, m):
 
 class Mortgage(object):
     """Abstract class for building different kinds of mortgages"""
-    def __init__(self):
-        pass
+    def __init__(self, loan, ann_rate, months):
+        """ Assumes: loan and ann_rate are floats, months is an int
+            Creates a new mortgage of size loan, duration months, and
+            annual interest rate ann_rate"""
+        self.loan = loan
+        self.rate = ann_rate / 12
+        self.months = months
+        self.paid = [0.0]
+        self.outstanding = [loan]
+        self.payment = find_payment(loan, self.rate, months)
+        self.legend = None # description of the mortgage
 
-    pass
+    def make_payment(self):
+        """Make a payment"""
+        self.paid.append(self.payment)
+        reduction = self.payment - (self.oustanding[-1] * self.rate)
+        self.outstanding.append(self.outstanding[-1] - reduction)
 
+    def get_total_paid(self):
+        """Determine total amount paid thus far"""
+        return sum(self.paid)
+
+    def __str__(self):
+        return self.legend
+        
 class Fixed(Mortgage):
     """Subclass of Mortgage to track fixed mortgage objects"""
     def __init__(self):
